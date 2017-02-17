5a45740526d586366b7933068969687f40d084ae
Olivier <oliviervandeneede@hotmail.com>
2017-02-17 16:32:54 +0100

    mainline kernel + aanpassingen dts

---
+++ code/i2c-test/test.c
@@ -0,0 +1,43 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void)
+{
+
+	int file;
+	char * filename = "/dev/i2c-1";
+
+
+	if ((file == open(filename, O_RDWR)) < 0)
+	{
+		printf("fout bij openen i2c interface\n");
+		exit(1);
+	}
+
+	int addr = 0b00101001;
+
+	if(ioctl(file, I2C_SLAVE, addr) < 0)
+	{
+		printf("Geen verbingeng met slave mogelijk\n");
+		exit(2);
+	}
+	
+	int buf = 0b10101010;
+
+	write(file, &buf, 1);
+	
+	write(file, &buf, 1);
+
+	write(file, &buf, 1);
+	printf("Gelukt!\n");
+
+	return 0;
+}
