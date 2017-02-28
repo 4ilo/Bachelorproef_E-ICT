ead5eb5cdd73ef234d1f048626740a1d378d8a0a
Olivier <oliviervandeneede@hotmail.com>
2017-02-28 10:14:15 +0100

    travis werkt?

--- Code/sim-knx/cross_compile.cmake
+++ Code/sim-knx/cross_compile.cmake
@@ -3,8 +3,8 @@ INCLUDE(CMakeForceCompiler)
 SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-#SET(GPP_COMPILER_FLAGS "-std=c++11")
-#add_definitions(${GPP_COMPILER_FLAGS})
+SET(GPP_COMPILER_FLAGS "-std=c++11")
+add_definitions(${GPP_COMPILER_FLAGS})
 
 # specify the cross compiler
 CMAKE_FORCE_C_COMPILER(/home/olivier/Documents/School/Bachelorproef/custom-linux/buildroot-2016.11.2/output/host/usr/bin/arm-buildroot-linux-uclibcgnueabi-gcc GNU)
